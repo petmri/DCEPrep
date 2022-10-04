8cacc2dcdaef8ea9689ef671b5d12a43ed8f82f0
Lucas Saca <LLSaca@llu.edu>
2022-10-04 15:05:39 -0700

    Add GitHub Actions testing pipeline to this repo, options added to script, split script into two steps (#2)
    * mcflirt plots fix, file should generate and first three params converted from rad to mm
    
    * add github actions workflow file
    
    * Update yolo.yml
    
    * how does self-hosting work
    
    * Update yolo.yml
    
    * me dum
    
    * add path to ROCKETSHIP checkout
    
    * fix path searching to return only one (correct) path)
    
    * add options to script, make runner data directory clean before every run
    
    * some kind of tab error or something in yml
    
    * me dum & forgor cd to path
    
    * attempted parenthesis problem fix?
    
    * spooky rm -f
    
    just in time for spooktober
    
    * try stuff to diagnose dumb error
    
    * attempted cope fix for parenthesis issue
    
    * COOOOOOOPE
    
    * try extglob for rm () issue
    
    * file existence checks with exit code for github actions
    
    * Update and rename yolo.yml to DCE.yml
    
    change branches that activate this workflow
    
    * exit after help option
    
    * split script into preprocessing and DCE steps
    
    * Update DCE.yml
    
    accommodate script split

--- max_disp.py
+++ max_disp.py
@@ -4,13 +4,7 @@ from numpy.core.multiarray import unravel_index
 
 dir = sys.argv[1]
 data = np.loadtxt(dir + "/DCE_mc.nii.par", dtype = float)
+data[:,0:3] = data[:,0:3]*50
 max_i = data.argmax()
 i = unravel_index(max_i, data.shape)
-if i[1] < 3:
-    print("Max displacement of " + str(data[i]) + " degrees at time slice " + str(i[0]) + ", parameter " + str(i[1]))
-elif i[1] == 3:
-    print("Max displacement of " + str(data[i]) + " mm at time slice " + str(i[0]) + ", parameter 3 (x)")
-elif i[1] == 4:
-    print("Max displacement of " + str(data[i]) + " mm at time slice " + str(i[0]) + ", parameter 4 (y)")
-elif i[1] == 5:
-    print("Max displacement of " + str(data[i]) + " mm at time slice " + str(i[0]) + ", parameter 5 (z)")
+print("Max displacement of " + str(data[i]) + " mm at time slice " + str(i[0] + 1) + "/64, parameter " + str(i[1]))
