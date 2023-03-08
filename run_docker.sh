f82edb9b8d1f823b8f8ac89ebb04862c6220385a
Lucas Saca <LLSaca@llu.edu>
2023-03-08 14:20:01 -0800

    increase matlab & freesurfer license path agnosticism in docker wrapper

--- run_docker.sh
+++ run_docker.sh
@@ -3,29 +3,28 @@
 
 # MAKE SURE MATLAB & FREESURFER LICENSE FILES, DATA DIRECTORY, AND /etc/ ARE SHARED TO DOCKER
 
-bozo=$(cat /usr/local/MATLAB/R2022a/licenses/*lic* | grep _HOSTID)
+bozo=$(cat /usr/local/MATLAB/*/licenses/*lic* | grep _HOSTID)
 
 MAC=${bozo#*MATLAB_HOSTID=}
 MAC=${MAC%%:*}
 MAC=${MAC:0:2}:${MAC:2:2}:${MAC:4:2}:${MAC:6:2}:${MAC:8:2}:${MAC:10:2}
-echo $MAC
+# echo $MAC
 
 MATLAB_PATH=/usr/local/MATLAB/$(ls /usr/local/MATLAB/)
-echo $MATLAB_PATH
+# echo $MATLAB_PATH
 
 LICENSE=$(ls $MATLAB_PATH/licenses)
-echo $LICENSE
+# echo $LICENSE
 
 export UID=$(id -u)
 export GID=$(id -g)
 
 sudo docker run --rm -it -e MLM_LICENSE_FILE=/opt/matlab/licenses/$LICENSE \
     -v /media/network_mriphysics/USC-PPG/docker_test:/data/ \
-    -v /home/mrispec/bin/freesurfer/license.txt:/opt/freesurfer/license.txt \
+    -v $FREESURFER_HOME/license.txt:/opt/freesurfer/license.txt \
     -v $MATLAB_PATH/licenses:/opt/matlab/licenses \
     -v /etc/passwd:/etc/passwd:ro \
     --shm-size=512M --mac-address $MAC \
     --user $UID:$GID \
     --gpus all \
-    final_test 
-    # sudo useradd -u `id -u` $USER
+    lsaca05/dce
