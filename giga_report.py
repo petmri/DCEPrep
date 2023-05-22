035fa09878fc9a0186a895c5515b3c85f7d87905
Lucas Saca <LLSaca@llu.edu>
2023-05-22 16:08:35 -0700

    let report run with missing files, replacing expected strings with error strings

--- giga_report.py
+++ giga_report.py
@@ -14,21 +14,32 @@ files_to_reorient = [dir + '/2.nii', dir + '/dce_patlak_fit_Ktrans.nii', dir + '
 for file in files_to_reorient:
     file_no_extension = file.split('.')[0]
     command = ['c3d', file, '-orient', 'RAS', '-o', file_no_extension + '_RAS.nii']
-    subprocess.run(command, check=True)
+    try:
+        subprocess.run(command, check=True)
+        reorient_success = True
+    except:
+        print("Error running c3d command: " + ' '.join(command))
+        print("Check if c3d is installed and in your path.")
+        reorient_success = False
 
-ktrans = nib.load(str(dir) + '/dce_patlak_fit_Ktrans_RAS.nii')
-ktrans_data = ktrans.get_fdata()
-ktrans_flipped = np.flip(ktrans_data, axis=1)
-ktrans_flipped = nib.Nifti1Image(ktrans_flipped, ktrans.affine, ktrans.header)
+if reorient_success:
+    ktrans = nib.load(str(dir) + '/dce_patlak_fit_Ktrans_RAS.nii')
+    ktrans_data = ktrans.get_fdata()
+    ktrans_flipped = np.flip(ktrans_data, axis=1)
+    ktrans_flipped = nib.Nifti1Image(ktrans_flipped, ktrans.affine, ktrans.header)
+    dimensions = ktrans.header.get_data_shape()
+    voxel_size = ktrans.header.get_zooms()
 
-
-# save plot of Ktrans
-fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(15, 5), gridspec_kw={'hspace': -.1, 'wspace': -.1}, dpi=300)
-expected_ktrans_vmax = 0.005
-plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-124, -159, -5), axes=axes[0], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False, colorbar=True)
-plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-159, -194, -5), axes=axes[1], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False)
-plt.savefig(str(dir) + '/ktrans.svg', bbox_inches='tight', pad_inches = 0)
-
+    # save plot of Ktrans
+    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(15, 5), gridspec_kw={'hspace': -.1, 'wspace': -.1}, dpi=300)
+    expected_ktrans_vmax = 0.005
+    plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-124, -159, -5), axes=axes[0], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False, colorbar=True)
+    plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-159, -194, -5), axes=axes[1], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False)
+    plt.savefig(str(dir) + '/ktrans.svg', bbox_inches='tight', pad_inches = 0)
+else:
+    print("Error plotting Ktrans")
+    dimensions = 'ktrans failed to load'
+    voxel_size = 'ktrans failed to load'
 
 # use jinja2 to generate html
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
@@ -42,94 +53,121 @@ commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('asci
 # get subject id
 subject_id = dir.split('/')[-2]
 # get institute from DCE.json
-with open(str(dir) + '/DCE.json') as f:
-    dce = json.load(f)
-institute = dce['InstitutionName']
-manufacturer = dce['Manufacturer']
-dimensions = ktrans.header.get_data_shape()
-voxel_size = ktrans.header.get_zooms()
+try:
+    with open(str(dir) + '/DCE.json') as f:
+        dce = json.load(f)
+    institute = dce['InstitutionName']
+    manufacturer = dce['Manufacturer']
+    MR_machine_model = dce['ManufacturersModelName']
+    field_strength = dce['MagneticFieldStrength']
+except:
+    dce = {}
+    institute = 'no json'
+    manufacturer = 'no json'
+    MR_machine_model = 'no json'
+    field_strength = 'no json'
 
-# brain mask
-plotting.plot_roi(str(dir) + '/T1_bet_mask.nii.gz', bg_img=str(dir)+'/T1.nii', vmin=0, vmax=1, cmap='gray', output_file=str(dir)+'/t1w_mask.svg', annotate=False, colorbar=False, draw_cross=False, title='mask')
+try:
+    # brain mask
+    plotting.plot_roi(str(dir) + '/T1_bet_mask.nii.gz', bg_img=str(dir)+'/T1.nii', vmin=0, vmax=1, cmap='gray', output_file=str(dir)+'/t1w_mask.svg', annotate=False, colorbar=False, draw_cross=False, title='mask')
 
-# T1w segmentation
-plotting.plot_anat(str(dir) + '/T1.nii', cmap='gray', output_file=str(dir)+'/t1w.svg', annotate=False, colorbar=False, draw_cross=False)
-plotting.plot_roi(str(dir) + '/segmented_t1_seg_2.nii.gz', bg_img=str(dir)+'/T1.nii', vmin=0, vmax=1, cmap='gray', output_file=str(dir)+'/t1w_wm.svg', annotate=False, colorbar=False, draw_cross=False, title='wm')
-plotting.plot_roi(str(dir) + '/segmented_t1_seg_1.nii.gz', bg_img=str(dir)+'/T1.nii', vmin=0, vmax=1, cmap='gray', output_file=str(dir)+'/t1w_gm.svg', annotate=False, colorbar=False, draw_cross=False, title='gm')
+    # T1w segmentation
+    plotting.plot_anat(str(dir) + '/T1.nii', cmap='gray', output_file=str(dir)+'/t1w.svg', annotate=False, colorbar=False, draw_cross=False)
+    plotting.plot_roi(str(dir) + '/segmented_t1_seg_2.nii.gz', bg_img=str(dir)+'/T1.nii', vmin=0, vmax=1, cmap='gray', output_file=str(dir)+'/t1w_wm.svg', annotate=False, colorbar=False, draw_cross=False, title='wm')
+    plotting.plot_roi(str(dir) + '/segmented_t1_seg_1.nii.gz', bg_img=str(dir)+'/T1.nii', vmin=0, vmax=1, cmap='gray', output_file=str(dir)+'/t1w_gm.svg', annotate=False, colorbar=False, draw_cross=False, title='gm')
+except:
+    print("Error plotting T1w segmentation")
 
-# T1w to VFA
-plotting.plot_anat(str(dir) + '/T1_wm_mask_RAS.nii', cmap='gray', output_file=str(dir)+'/t1w_to_vfa.svg', cut_coords=7, display_mode='z', annotate=False, colorbar=False, draw_cross=False, title='T1w to VFA')
+if reorient_success:
+    # T1w to VFA
+    plotting.plot_anat(str(dir) + '/T1_wm_mask_RAS.nii', cmap='gray', output_file=str(dir)+'/t1w_to_vfa.svg', cut_coords=7, display_mode='z', annotate=False, colorbar=False, draw_cross=False, title='T1w to VFA')
 
-# T1w to dyn
-plotting.plot_anat(str(dir) + '/T1_bet_mask_dyn_RAS.nii', cmap='gray', output_file=str(dir)+'/t1bet_to_dyn.svg', cut_coords=7, display_mode='z', annotate=False, colorbar=False, draw_cross=False, title='T1w brain mask to dyn')
-plotting.plot_anat(str(dir) + '/T1_wm_mask_dyn_RAS.nii', cmap='gray', output_file=str(dir)+'/t1wm_to_dyn.svg', cut_coords=7, display_mode='z', annotate=False, colorbar=False, draw_cross=False, title='T1w wm to dyn')
-plotting.plot_anat(str(dir) + '/T1_gm_mask_dyn_RAS.nii', cmap='gray', output_file=str(dir)+'/t1gm_to_dyn.svg', cut_coords=7, display_mode='z', annotate=False, colorbar=False, draw_cross=False, title='T1w gm to dyn')
+    # T1w to dyn
+    plotting.plot_anat(str(dir) + '/T1_bet_mask_dyn_RAS.nii', cmap='gray', output_file=str(dir)+'/t1bet_to_dyn.svg', cut_coords=7, display_mode='z', annotate=False, colorbar=False, draw_cross=False, title='T1w brain mask to dyn')
+    plotting.plot_anat(str(dir) + '/T1_wm_mask_dyn_RAS.nii', cmap='gray', output_file=str(dir)+'/t1wm_to_dyn.svg', cut_coords=7, display_mode='z', annotate=False, colorbar=False, draw_cross=False, title='T1w wm to dyn')
+    plotting.plot_anat(str(dir) + '/T1_gm_mask_dyn_RAS.nii', cmap='gray', output_file=str(dir)+'/t1gm_to_dyn.svg', cut_coords=7, display_mode='z', annotate=False, colorbar=False, draw_cross=False, title='T1w gm to dyn')
 
-# T1 map
-# read txt file
-FAs = []
-is_target_line = False
-with open(str(dir) + '/T1_map_t1_fa_fit_VFA_mc.txt', 'r') as f:
-    for line in f:
-        if "User selected TE/TR/FA/TI: " in line:
-            is_target_line = True
-        elif is_target_line:
-            match = re.search(r'\d+', line)
-            if match:
-                number = int(match.group())
-                FAs.append(number)
-            else:
-                is_target_line = False
-# take last set of non-repeating numbers
-FAs = FAs[-5:]
-# convert from list to string
-FA_str = [str(i) for i in FAs]
-FA_str = ', '.join(FA_str)
+try:
+    # T1 map
+    # read txt file
+    FAs = []
+    is_target_line = False
+    with open(str(dir) + '/T1_map_t1_fa_fit_VFA_mc.txt', 'r') as f:
+        for line in f:
+            if "User selected TE/TR/FA/TI: " in line:
+                is_target_line = True
+            elif is_target_line:
+                match = re.search(r'\d+', line)
+                if match:
+                    number = int(match.group())
+                    FAs.append(number)
+                else:
+                    is_target_line = False
+    # take last set of non-repeating numbers
+    FAs = FAs[-5:]
+    # convert from list to string
+    FA_str = [str(i) for i in FAs]
+    FA_str = ', '.join(FA_str)
 
-# now get TR from txt file
-TR = None
-is_target_line = False
-with open(str(dir) + '/T1_map_t1_fa_fit_VFA_mc.txt', 'r') as f:
-    for line in f:
-        if "User selected tr: " in line:
-            is_target_line = True
-        elif is_target_line:
-            match = re.search(r'\d+.\d+', line)
-            if match:
-                TR = match.group()
-            else:
-                is_target_line = False
+    # now get TR from txt file
+    TR = None
+    is_target_line = False
+    with open(str(dir) + '/T1_map_t1_fa_fit_VFA_mc.txt', 'r') as f:
+        for line in f:
+            if "User selected tr: " in line:
+                is_target_line = True
+            elif is_target_line:
+                match = re.search(r'\d+.\d+', line)
+                if match:
+                    TR = match.group()
+                else:
+                    is_target_line = False
 
-# check if GPU was used
-GPU = False
-with open(str(dir) + '/T1_map_t1_fa_fit_VFA_mc.txt', 'r') as f:
-    for line in f:
-        if "GPU detected" in line:
-            GPU = True
-if GPU:
-    GPU_T1 = 'GPU was used'
-else:
-    GPU_T1 = 'CPU was used'
+    # check if GPU was used
+    GPU = False
+    with open(str(dir) + '/T1_map_t1_fa_fit_VFA_mc.txt', 'r') as f:
+        for line in f:
+            if "GPU detected" in line:
+                GPU = True
+    if GPU:
+        GPU_T1 = 'GPU was used'
+    else:
+        GPU_T1 = 'CPU was used'
+except:
+    print("Error getting T1 map parameters")
+    FAs = [-1, -1, -1, -1, -1]
+    FA_str = 'Failed to load FAs'
+    TR = -1
+    GPU_T1 = 'Failed to load GPU info'
 
-# T1 map
-plotting.plot_anat(str(dir) + '/t1_map_fixed_use_me.nii.gz', cmap='gray', output_file=str(dir)+'/t1_map.svg', annotate=False, colorbar=False, draw_cross=False, title='T1 map')
+try:
+    # T1 map
+    plotting.plot_anat(str(dir) + '/t1_map_fixed_use_me.nii.gz', cmap='gray', output_file=str(dir)+'/t1_map.svg', annotate=False, colorbar=False, draw_cross=False, title='T1 map')
+except:
+    print("Error plotting T1 map")
 
-# AIF
-plotting.plot_roi(str(dir) + '/aif.nii', bg_img=str(dir)+'/15.nii', vmin=0, vmax=1, cmap='gray', output_file=str(dir)+'/AIF.svg', annotate=False, colorbar=False, draw_cross=False, title='AIF')
+try:
+    # AIF
+    plotting.plot_roi(str(dir) + '/aif.nii', bg_img=str(dir)+'/15.nii', vmin=0, vmax=1, cmap='gray', output_file=str(dir)+'/AIF.svg', annotate=False, colorbar=False, draw_cross=False, title='AIF')
+except:
+    print("Error plotting AIF")
 
 # T1 dynamic space
 
 # get DCE parameters
-import re
-
 def extract_value(pattern, text):
     match = re.search(pattern, text)
     if match:
         return match.group(1)
     return None
 
-with open(dir + '/A_dceR1info.log', 'r') as file:
-    log_text = file.read()
+try:
+    with open(dir + '/A_dceR1info.log', 'r') as file:
+        log_text = file.read()
+    RUNA_log = True
+except:
+    log_text = ''
+    RUNA_log = False
 
 tr_pattern = r"User selected TR \(ms\):\s+(\d+\.\d+)"
 fa_pattern = r"User selected FA \(degrees\):\s+(\d+)"
@@ -145,83 +183,105 @@ snr_threshold = extract_value(snr_threshold_pattern, log_text)
 relaxivity = extract_value(relaxivity_pattern, log_text)
 steady_state = extract_value(steady_state_pattern, log_text)
 
-# get last line of log file
-with open(dir + '/A_dceR1info.log', 'r') as file:
-    for line in file:
-        pass
-    A_last_line = line
-
-# now get Time Resolution from log file
-time_resolution = None
-is_target_line = False
-with open(str(dir) + '/B_dcefitted_R1info.log', 'r') as f:
-    for line in f:
-        if "User selected time resolution (sec)" in line:
-            is_target_line = True
-        elif is_target_line:
-            match = re.search(r'(\d+)(.*)\d*', line)
-            if match:
-                time_resolution = match.group()
-            else:
-                is_target_line = False
-
-# now get R^2 from log file
-r_squared = None
-is_target_line = False
-with open(str(dir) + '/B_dcefitted_R1info.log', 'r') as f:
-    for line in f:
-        if "User selected time resolution (sec)" in line:
-            is_target_line = True
-        elif is_target_line:
-            match = re.search(r'(\d+)(.*)\d*', line)
-            if match:
-                r_squared = match.group()
-            else:
-                is_target_line = False
-
-def extract_r2_values(log_text):
-    r2_pattern = r"R\^2 of AIF fit = (\d+\.\d+)"
-    r2_values = re.findall(r2_pattern, log_text)
-    return r2_values
-
-with open(dir + '/B_dcefitted_R1info.log', 'r') as file:
-    log_text = file.read()
-
-r2_values = extract_r2_values(log_text)
-
-if len(r2_values) >= 2:
-    r2_aif_fit = r2_values[-2]
-    r2_raw_values = r2_values[-1]
-
-# get last line of B log file (time elapsed)
-with open(dir + '/B_dcefitted_R1info.log', 'r') as file:
-    for line in file:
-        pass
-    B_last_line = line
-
-# get GPU info
-GPU = False
-with open(str(dir) + '/dce_patlak_fit.log', 'r') as f:
-    for line in f:
-        if "Gpufit detected" in line:
-            GPU = True
-if GPU:
-    GPU_DCE = 'GPU was used'
+if RUNA_log:
+    # get last line of log file
+    with open(dir + '/A_dceR1info.log', 'r') as file:
+        for line in file:
+            pass
+        A_last_line = line
 else:
-    GPU_DCE = 'CPU was used'
+    A_last_line = 'Failed to load RUNA log'
 
-# get RUN D time elapsed
-def extract_elapsed_time(log_text):
-    elapsed_time_pattern = r"Elapsed time is (\d+\.\d+) seconds."
-    match = re.search(elapsed_time_pattern, log_text)
-    if match:
-        return match.group(1)
-    return None
+try:
+    # now get Time Resolution from log file
+    time_resolution = None
+    is_target_line = False
+    with open(str(dir) + '/B_dcefitted_R1info.log', 'r') as f:
+        for line in f:
+            if "User selected time resolution (sec)" in line:
+                is_target_line = True
+            elif is_target_line:
+                match = re.search(r'(\d+)(.*)\d*', line)
+                if match:
+                    time_resolution = match.group()
+                else:
+                    is_target_line = False
 
-with open(dir + '/dce_patlak_fit.log', 'r') as file:
-    log_text = file.read()
+    # now get R^2 from log file
+    r_squared = None
+    is_target_line = False
+    with open(str(dir) + '/B_dcefitted_R1info.log', 'r') as f:
+        for line in f:
+            if "User selected time resolution (sec)" in line:
+                is_target_line = True
+            elif is_target_line:
+                match = re.search(r'(\d+)(.*)\d*', line)
+                if match:
+                    r_squared = match.group()
+                else:
+                    is_target_line = False
+    RUNB_log = True
+except:
+    print("Error getting DCE RUNB parameters")
+    RUNB_log = False
 
-dce_elapsed_time = extract_elapsed_time(log_text)
+if RUNB_log:
+    def extract_r2_values(log_text):
+        r2_pattern = r"R\^2 of AIF fit = (\d+\.\d+)"
+        r2_values = re.findall(r2_pattern, log_text)
+        return r2_values
+
+    with open(dir + '/B_dcefitted_R1info.log', 'r') as file:
+        log_text = file.read()
+
+    r2_values = extract_r2_values(log_text)
+
+    if len(r2_values) >= 2:
+        r2_aif_fit = r2_values[-2]
+        r2_raw_values = r2_values[-1]
+
+    # get last line of B log file (time elapsed)
+    with open(dir + '/B_dcefitted_R1info.log', 'r') as file:
+        for line in file:
+            pass
+        B_last_line = line
+else:
+    r2_aif_fit = 'Failed to load RUNB log'
+    r2_raw_values = 'Failed to load RUNB log'
+    B_last_line = 'Failed to load RUNB log'
+
+try:
+    # get GPU info
+    GPU = False
+    with open(str(dir) + '/dce_patlak_fit.log', 'r') as f:
+        for line in f:
+            if "Gpufit detected" in line:
+                GPU = True
+    if GPU:
+        GPU_DCE = 'GPU was used'
+    else:
+        GPU_DCE = 'CPU was used'
+    RUND_log = True
+except:
+    print("Error getting DCE GPU info")
+    RUND_log = False
+    GPU_DCE = 'Failed to load GPU info'
+
+if RUND_log:
+    # get RUN D time elapsed
+    def extract_elapsed_time(log_text):
+        elapsed_time_pattern = r"Elapsed time is (\d+\.\d+) seconds."
+        match = re.search(elapsed_time_pattern, log_text)
+        if match:
+            return match.group(1)
+        return None
+
+    with open(dir + '/dce_patlak_fit.log', 'r') as file:
+        log_text = file.read()
+
+    dce_elapsed_time = extract_elapsed_time(log_text)
+else:
+    dce_elapsed_time = 'Failed to load RUND log'
 
 # Ktrans
 
@@ -232,7 +292,7 @@ data = {
     'Date': 'Date: ' + date,
     'Commit': 'Commit: ' + commit_hash,
     'Institute': 'Institute: ' + institute,
-    'Machine': 'Machine: ' + manufacturer + ' ' + dce['ManufacturersModelName'] + ' ' + str(dce['MagneticFieldStrength']) + 'T',
+    'Machine': 'Machine: ' + manufacturer + ' ' + MR_machine_model + ' ' + field_strength + 'T',
     'ktrans': dir + '/ktrans.svg',
     'image_path1': dir + '/dceAIF_fitting.png',
     'image_alt1': 'My image',
