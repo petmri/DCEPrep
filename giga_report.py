6a976cf412b9c7de5c7b5193be9fbae1c8d74f13
Lucas Saca <LLSaca@llu.edu>
2023-07-18 13:01:59 -0700

    try mri_convert extension fix (.nii.gz), stop doing ktrans stuff with every file reorientation

--- giga_report.py
+++ giga_report.py
@@ -20,20 +20,21 @@ if subprocess.run(['which', 'c3d'], stdout=subprocess.PIPE).returncode == 0:
         command = ['c3d', file, '-orient', 'RAS', '-o', file_no_extension + '_RAS.nii']
         try:
             subprocess.run(command, check=True)
-            ktrans = nib.load(str(dir) + '/dce_patlak_fit_Ktrans_RAS.nii')
-            ktrans_data = ktrans.get_fdata()
-            ktrans_flipped = np.flip(ktrans_data, axis=1)
-            ktrans_flipped = nib.Nifti1Image(ktrans_flipped, ktrans.affine, ktrans.header)
-            dimensions = ktrans.header.get_data_shape()
-            voxel_size = ktrans.header.get_zooms()
+            if file == dir + '/dce_patlak_fit_Ktrans.nii':
+                ktrans = nib.load(str(dir) + '/dce_patlak_fit_Ktrans_RAS.nii')
+                ktrans_data = ktrans.get_fdata()
+                ktrans_flipped = np.flip(ktrans_data, axis=1)
+                ktrans_flipped = nib.Nifti1Image(ktrans_flipped, ktrans.affine, ktrans.header)
+                dimensions = ktrans.header.get_data_shape()
+                voxel_size = ktrans.header.get_zooms()
 
-            # plot ktrans
-            fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(15, 5), gridspec_kw={'hspace': -.1, 'wspace': -.1}, dpi=300)
-            expected_ktrans_vmax = 0.005
-            plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-119, -154, -5), axes=axes[0], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False, colorbar=True)
-            plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-154, -189, -5), axes=axes[1], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False)
-            plt.savefig(str(dir) + '/figures/ktrans.svg', bbox_inches='tight', pad_inches = 0)
-            plt.close()
+                # plot ktrans
+                fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(15, 5), gridspec_kw={'hspace': -.1, 'wspace': -.1}, dpi=300)
+                expected_ktrans_vmax = 0.005
+                plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-119, -154, -5), axes=axes[0], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False, colorbar=True)
+                plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-154, -189, -5), axes=axes[1], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False)
+                plt.savefig(str(dir) + '/figures/ktrans.svg', bbox_inches='tight', pad_inches = 0)
+                plt.close()
         except Exception as e:
             print("Error running c3d command: " + ' '.join(command))
             # print("Check if c3d is installed and in your path, or if the target file exists.")
@@ -42,25 +43,26 @@ else:
 # use freesurfer's mri_convert to reorient files to RAS
     for file in files_to_reorient:
         file_no_extension = file.split('.')[0]
-        command = ['mri_convert', '--in_orientation', 'LPI', file, file_no_extension + '_RAS.nii']
+        command = ['mri_convert', '--in_orientation', 'LPI', file, file_no_extension + '_RAS.nii.gz']
         try:
             subprocess.run(command, check=True)
-            ktrans = nib.load(str(dir) + '/dce_patlak_fit_Ktrans_RAS.nii')
-            ktrans_data = ktrans.get_fdata()
-            ktrans_flipped = np.flip(ktrans_data, axis=1)
-            ktrans_flipped = nib.Nifti1Image(ktrans_flipped, ktrans.affine, ktrans.header)
-            dimensions = ktrans.header.get_data_shape()
-            voxel_size = ktrans.header.get_zooms()
+            if file == dir + '/dce_patlak_fit_Ktrans.nii':
+                ktrans = nib.load(str(dir) + '/dce_patlak_fit_Ktrans_RAS.nii.gz')
+                ktrans_data = ktrans.get_fdata()
+                ktrans_flipped = np.flip(ktrans_data, axis=1)
+                ktrans_flipped = nib.Nifti1Image(ktrans_flipped, ktrans.affine, ktrans.header)
+                dimensions = ktrans.header.get_data_shape()
+                voxel_size = ktrans.header.get_zooms()
 
-            # plot Ktrans, different coords
-            fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(15, 5), gridspec_kw={'hspace': -.1, 'wspace': -.1}, dpi=300)
-            expected_ktrans_vmax = 0.005
-            plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-56, -21, 5), axes=axes[0], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False, colorbar=True)
-            plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-21, 13, 5), axes=axes[1], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False)
-            plt.savefig(str(dir) + '/figures/ktrans.svg', bbox_inches='tight', pad_inches = 0)
-            plt.close()
+                # plot Ktrans, different coords
+                fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(15, 5), gridspec_kw={'hspace': -.1, 'wspace': -.1}, dpi=300)
+                expected_ktrans_vmax = 0.005
+                plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-56, -21, 5), axes=axes[0], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False, colorbar=True)
+                plotting.plot_anat(ktrans_flipped, display_mode='z', cut_coords=range(-21, 13, 5), axes=axes[1], vmin=0, vmax=expected_ktrans_vmax, cmap='gnuplot', annotate=False)
+                plt.savefig(str(dir) + '/figures/ktrans.svg', bbox_inches='tight', pad_inches = 0)
+                plt.close()
         except Exception as e:
-            print("Error plotting Ktrans")
+            print("Error running freesurfer mri_convert (reorient)")
             dimensions = 'ktrans failed to load'
             voxel_size = 'ktrans failed to load'
             print(e)
